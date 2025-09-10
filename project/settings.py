7b590a9227f66805b95505232859d2cb2a1a829e
bobby-bose <bobbykboseoffice@gmail.com>
2025-09-11 00:45:36 +0530

    dfdf

--- project/settings.py
+++ project/settings.py
@@ -15,9 +15,7 @@ SECRET_KEY = 'fgfdgdfgfgfgggggggggggggggggggdfgfgfg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '*'
-]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [

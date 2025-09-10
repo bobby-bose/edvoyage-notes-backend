08e884c85d6ed1ad0f824b12a37b56e8f28859c2
bobby-bose <bobbykboseoffice@gmail.com>
2025-09-10 23:11:49 +0530

    first commit

--- project/settings.py
+++ project/settings.py
@@ -132,3 +132,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# URL to serve static files from.
+STATIC_URL = '/static/'
+
+# Add your app's static files directories.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+

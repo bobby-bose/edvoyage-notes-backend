cdae5cbaa32bf5b8ba52e7ab603610d229b1906c
bobby-bose <bobbykboseoffice@gmail.com>
2025-09-10 23:08:46 +0530

    first commit

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jpuvggl=arkxm@c3_sx*=t8(5xik@k^s06eq8(=gt6q&wakkjh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['edvoyagebackend.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
